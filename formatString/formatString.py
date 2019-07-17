0ecdfc4a4d9d13565438d6882c537d03639efbee
wyt1996 <ibewhour@163.com>
2019-07-17 18:54:24 +0800

    format函数使用

---
+++ formatString/formatString.py
@@ -0,0 +1,5 @@
+print('{} a word she can get what she {} for.'.format('With','came'))
+
+#利用api查询天气
+#city = input('write down the name of city:')
+#url = 'http://apistore.baidu.com/microservice/weather?citypinyin={}'.format(city)
